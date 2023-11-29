361bc4b84fae33ffae9b1e44340da71d41b89b47
Nachichuri <nachichuri@gmail.com>
2023-11-29 01:16:15 -0300

    refactor: update code with dynamic paths to prevent code duplication

--- api/main.py
+++ api/main.py
@@ -6,7 +6,9 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse, StreamingResponse
 from PIL import Image, ImageSequence
 
-from api.helpers import api_description, get_latest_version, get_resized_gif
+from api.documentation import api_description
+from api.helpers import get_latest_version, get_resized_gif
+
 
 app = FastAPI(
     title="🏗️ build-result-meme-api",
@@ -22,51 +24,45 @@ app = FastAPI(
 
 @app.get("/", response_class=RedirectResponse, status_code=303, include_in_schema=False)
 async def docs():
-    return "/docs"
+    """
+    Redirects the root URL to the '/docs' endpoint with a status code of 303 (See Other).
+    """
+    return RedirectResponse("/docs")
 
 
-@app.get("/success", tags=["memes"])
-async def success():
-    random_image = (
-        f'api/assets/success/{random.choice(os.listdir("api/assets/success/"))}'
-    )
+@app.get("/{status}", include_in_schema=False)
+@app.get("/{status}/{size}", tags=["Endpoints"])
+async def meme(status: str, size: str = "500") -> StreamingResponse:
+    """
+    ## Generates and returns a meme image or GIF based on the specified status and size.
 
-    if random_image[-3:] == "gif":
-        source = Image.open(random_image)
-        frames = ImageSequence.Iterator(source)
-        frames = get_resized_gif(frames, (500, 500))
+    ### Args:\n
+    `status (str)`: The category or status of the meme.\n
+    `size (str, optional)`: The desired size of the meme. Defaults to "500".
 
-        processed_gif = next(frames)
-        processed_gif.info = source.info
-        final_gif = BytesIO()
-        processed_gif.save(final_gif, "GIF", save_all=True, append_images=list(frames))
-        final_gif.seek(0)
+    ### Returns:\n
+    `fastapi.responses.StreamingResponse`: A streaming response containing the generated meme.
 
-        return StreamingResponse(final_gif, media_type="image/gif")
+    ### Raises:\n
+    `fastapi.responses.RedirectResponse`: Redirects to the '/docs' endpoint if the specified status is not valid.
+    """
+    # Check if the specified status exists in the meme assets directory
+    if status not in os.listdir("api/assets/"):
+        return RedirectResponse("/docs", status_code=303)
 
-    source = Image.open(random_image)
-    source.thumbnail((500, 500))
-    source = source.convert("RGB")
-
-    image = BytesIO()
-    source.save(image, "JPEG")
-    image.seek(0)
-
-    return StreamingResponse(image, media_type="image/jpeg")
-
-
-@app.get("/success/{size}", tags=["custom size"])
-async def success_custom(size):
+    # Attempt to convert the size to an integer; default to 500 if conversion fails
     try:
         size = int(size)
-    except Exception:
+    except ValueError:
         size = 500
 
+    # Form the path to a random image within the specified status directory
     random_image = (
-        f'api/assets/success/{random.choice(os.listdir("api/assets/success/"))}'
+        f'api/assets/{status}/{random.choice(os.listdir(f"api/assets/{status}/"))}'
     )
 
-    if random_image[-3:] == "gif":
+    # If the randomly chosen image is a GIF, process and return a streaming GIF response
+    if random_image.lower().endswith("gif"):
         source = Image.open(random_image)
         frames = ImageSequence.Iterator(source)
         frames = get_resized_gif(frames, (size, size))
@@ -79,6 +75,7 @@ async def success_custom(size):
 
         return StreamingResponse(final_gif, media_type="image/gif")
 
+    # If the randomly chosen image is not a GIF, process and return a streaming JPEG response
     source = Image.open(random_image)
     source.thumbnail((size, size))
     source = source.convert("RGB")
@@ -90,127 +87,16 @@ async def success_custom(size):
     return StreamingResponse(image, media_type="image/jpeg")
 
 
-@app.get("/fixed", tags=["memes"])
-async def fixed():
-    random_image = f'api/assets/fixed/{random.choice(os.listdir("api/assets/fixed/"))}'
+@app.exception_handler(404)
+async def custom_404_handler(_, __):
+    """
+    Custom exception handler for handling 404 Not Found errors.
 
-    if random_image[-3:] == "gif":
-        source = Image.open(random_image)
-        frames = ImageSequence.Iterator(source)
-        frames = get_resized_gif(frames, (500, 500))
+    Args:
+        _: FastAPI request object (not used in the function).
+        __: Exception information (not used in the function).
 
-        processed_gif = next(frames)
-        processed_gif.info = source.info
-        final_gif = BytesIO()
-        processed_gif.save(final_gif, "GIF", save_all=True, append_images=list(frames))
-        final_gif.seek(0)
-
-        return StreamingResponse(final_gif, media_type="image/gif")
-
-    source = Image.open(random_image)
-    source.thumbnail((500, 500))
-    source = source.convert("RGB")
-
-    image = BytesIO()
-    source.save(image, "JPEG")
-    image.seek(0)
-
-    return StreamingResponse(image, media_type="image/jpeg")
-
-
-@app.get("/fixed/{size}", tags=["custom size"])
-async def fixed_custom(size):
-    try:
-        size = int(size)
-    except Exception:
-        size = 500
-
-    random_image = f'api/assets/fixed/{random.choice(os.listdir("api/assets/fixed/"))}'
-
-    if random_image[-3:] == "gif":
-        source = Image.open(random_image)
-        frames = ImageSequence.Iterator(source)
-        frames = get_resized_gif(frames, (size, size))
-
-        processed_gif = next(frames)
-        processed_gif.info = source.info
-        final_gif = BytesIO()
-        processed_gif.save(final_gif, "GIF", save_all=True, append_images=list(frames))
-        final_gif.seek(0)
-
-        return StreamingResponse(final_gif, media_type="image/gif")
-
-    source = Image.open(random_image)
-    source.thumbnail((size, size))
-    source = source.convert("RGB")
-
-    image = BytesIO()
-    source.save(image, "JPEG")
-    image.seek(0)
-
-    return StreamingResponse(image, media_type="image/jpeg")
-
-
-@app.get("/failure", tags=["memes"])
-async def failure():
-    random_image = (
-        f'api/assets/failure/{random.choice(os.listdir("api/assets/failure/"))}'
-    )
-
-    if random_image[-3:] == "gif":
-        source = Image.open(random_image)
-        frames = ImageSequence.Iterator(source)
-        frames = get_resized_gif(frames, (500, 500))
-
-        processed_gif = next(frames)
-        processed_gif.info = source.info
-        final_gif = BytesIO()
-        processed_gif.save(final_gif, "GIF", save_all=True, append_images=list(frames))
-        final_gif.seek(0)
-
-        return StreamingResponse(final_gif, media_type="image/gif")
-
-    source = Image.open(random_image)
-    source.thumbnail((500, 500))
-    source = source.convert("RGB")
-
-    image = BytesIO()
-    source.save(image, "JPEG")
-    image.seek(0)
-
-    return StreamingResponse(image, media_type="image/jpeg")
-
-
-@app.get("/failure/{size}", tags=["custom size"])
-async def failure_custom(size):
-    try:
-        size = int(size)
-    except Exception:
-        size = 500
-
-    random_image = (
-        f'api/assets/failure/{random.choice(os.listdir("api/assets/failure/"))}'
-    )
-
-    if random_image[-3:] == "gif":
-        source = Image.open(random_image)
-        frames = ImageSequence.Iterator(source)
-        frames = get_resized_gif(frames, (size, size))
-
-        processed_gif = next(frames)
-        processed_gif.info = source.info
-        final_gif = BytesIO()
-        processed_gif.save(final_gif, "GIF", save_all=True, append_images=list(frames))
-        final_gif.seek(0)
-
-        return StreamingResponse(final_gif, media_type="image/gif")
-
-    source = Image.open(random_image)
-    source.thumbnail((size, size))
-    source = source.convert("RGB")
-
-    image = BytesIO()
-    source.save(image, "JPEG")
-    image.seek(0)
-
-    return StreamingResponse(image, media_type="image/jpeg")
+    Returns:
+        RedirectResponse: Redirects to the '/docs' endpoint with a status code of 303 (See Other).
+    """
+    return RedirectResponse("/docs", status_code=303)
